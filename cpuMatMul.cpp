1ef17934886917c3cd847042054cb77df923ae09
Asiwaju-Adeniyi <makindeadeniyi1@gmail.com>
2025-12-16 15:17:18 +0100

    Update cpuMatMul.cpp

--- cpuMatMul.cpp
+++ cpuMatMul.cpp
@@ -10,6 +10,7 @@ class MatrixFP32{
     const int n_cols;
 
     float *ptr;
+    const bool on_device;
 
     MatrixFP32(int n_rows, int n_cols);
 
@@ -17,11 +18,39 @@ class MatrixFP32{
 };
 
 MatrixFP32::MatrixFP32(int n_rows_, int n_cols) : n_rows(n_rows_), n_cols(n_cols){
+    if (on_device == false ) {
+
     ptr = new float[n_rows * n_cols];
+    } else {
+        cudaError_t err = cudaMalloc((void**) &ptr, n_rows * n_cols * sizeof(float));
+    }
 }
 
 void MatrixFP32::Free_Mat(){
+
+    if (on_device == false)
      delete[] ptr;
+} else {
+    cudaFree(ptr);
+}
+
+void MatrixFP32::DeviceCopy(MatrixFP32 d_Mat){
+assert(on_device == false && "Matrix must be in host memory");
+assert(d_mat.on_device == true && "Input Matrix to this function must be in device memory");
+
+cudaError_t err = cudaMemcpy(d_mat.ptr, ptr, n_rows*n_cols*sizeof(float), cudaMemcpyHostToDevice);
+cuda_check(err);
+}
+
+void MatrixFP32::copy_to_host(MatrixFP32 h_mat)
+{
+
+    assert(on_device == true && "Matrix must be in device memory");
+    assert(h_mat.on_device == false && "Input Matrix to this function must be in host memory");
+
+    
+    cudaError_t err = cudaMemcpy(h_mat.ptr, ptr, n_rows*n_cols*sizeof(float), cudaMemcpyDeviceToHost);
+    cuda_check(err);
 }
 
 
